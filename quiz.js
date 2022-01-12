dfdb9ae7ac68c61e9e15a247815aac67a418b67f
Ranjeetk489 <ranjeet07494@gmail.com>
2022-01-12 06:45:26 +0530

    added files

--- quiz.js
+++ quiz.js
@@ -1,26 +1,33 @@
 const quizForm = document.querySelector(".form");
 const subBtn = document.querySelector("#subBtn");
-const output =  document.querySelector("#output")
+const output = document.querySelector("#output");
+const calculate = document.querySelector("#form");
 output.style.display = "none";
-const correctAnswers = ["Midsegment" , "75°", "scalene","isosceles triangle","right angle triangle"];
+const correctAnswers = [
+  "Midsegment",
+  "75°",
+  "scalene",
+  "isosceles triangle",
+  "right angle triangle",
+];
 
-function calculateScore(){
-    let score = 0;
-    let index  = 0;
-    const formResults = new FormData(quizForm);
-    for(let value of formResults.values()){
-        if(value === correctAnswers[index]){
-        score = score + 1;
+function calculateScore() {
+  let score = 0;
+  let index = 0;
+  const formResults = new FormData(quizForm);
+  for (let value of formResults.values()) {
+    if (value === correctAnswers[index]) {
+      score = score + 1;
     }
     index = index + 1;
+  }
+
+  output.innerText = "Your score is " + score;
 }
 
-output.innerText = "Your score is " + score;
-}
- 
-function showOutput(){
-    output.style.display = "block";
-    output.style.color = "green";
+function showOutput() {
+  output.style.display = "block";
+  output.style.color = "green";
 }
 
 subBtn.addEventListener("click", calculateScore);
